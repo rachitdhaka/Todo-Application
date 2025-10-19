54396a35d696986d88bcc9be23e0c4a7bac64618
Rachit Dhaka <rachitdhaka29@gmail.com>
2025-10-19 12:56:01 +0530

    hm

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,20 +19,35 @@ const Navbar = () => {
         localStorage.removeItem('token');
         localStorage.removeItem('name');
 
-        
+
         navigate('/login');
     }
+
+    const login = () => {
+        navigate('/login');
+    }
+
+    const isLoggedIn = localStorage.getItem('token');
 
     return (
         <div className='w-6xl mx-auto py-3 flex justify-end gap-4'>
 
-            <p className='px-2 py-1 rounded-md font-medium text-gray-700'>
-                Welcome, {name}
-            </p>
-            <button className='px-2 py-1 rounded-md bg-red-700 text-white cursor-pointer hover:bg-red-800 transition-colors'
-            onClick={logout}>
-                Logout
-            </button>
+            {isLoggedIn ? (
+                <>
+                    <p className='px-2 py-1 rounded-md font-medium text-gray-700'>
+                        Welcome, {name}
+                    </p>
+                    <button className='px-2 py-1 rounded-md bg-red-700 text-white cursor-pointer hover:bg-red-800 transition-colors'
+                    onClick={logout}>
+                        Logout
+                    </button>
+                </>
+            ) : (
+                <button className='px-2 py-1 rounded-md bg-green-600 text-white cursor-pointer hover:bg-green-700 transition-colors'
+                onClick={login}>
+                    Log In
+                </button>
+            )}
         </div>
     )
 }
